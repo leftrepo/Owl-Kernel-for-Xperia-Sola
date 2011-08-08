0ce685d1b444c32d3b24fd06f752ca4c779e5c53
Sujith Reddy Thumma <sthumma@codeaurora.org>
2011-08-08 12:59:48 +0530

    mmc: core: Skip frequency retries for SDCC slots
    Qualcomm SDCC controller supports minimum SD clock frequency
    which is required for card initialization. This information is
    exported through platform data for each SDCC controller. There is
    no need of retrying higher frequencies than the minimum supported
    by controller for Qualcomm chipsets which inturn add delay in
    detection process if there is no card during suspend/resume cycles.
    Hence, skip multiple frequency retries.
    
    Signed-off-by: Sujith Reddy Thumma <sthumma@codeaurora.org>
    
    Conflicts:
    	drivers/mmc/core/core.c

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2059,10 +2059,9 @@ EXPORT_SYMBOL(mmc_detect_card_removed);
 
 void mmc_rescan(struct work_struct *work)
 {
-	static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
 	struct mmc_host *host =
 		container_of(work, struct mmc_host, detect.work);
-	int i;
+	bool extend_wakelock = false;
 
 	if (host->rescan_disable || mmc_host_needs_resume(host))
 		return;
@@ -2108,12 +2107,8 @@ void mmc_rescan(struct work_struct *work)
 		goto out;
 
 	mmc_claim_host(host);
-	for (i = 0; i < ARRAY_SIZE(freqs); i++) {
-		if (!mmc_rescan_try_freq(host, max(freqs[i], host->f_min)))
-			break;
-		if (freqs[i] <= host->f_min)
-			break;
-	}
+	if (!mmc_rescan_try_freq(host, host->f_min))
+		extend_wakelock = true;
 	mmc_release_host(host);
 
  out:
