6062939d5e353327b30876ecd9ff5aebae002e29
Russell King <rmk+kernel@arm.linux.org.uk>
2011-09-18 11:27:30 +0100

    ARM: cache-l2x0.c: consistently use u32
    __u32 exists to avoid namespace clashes with userspace programs.  It
    should not be used outside header files, so convert to use u32 instead.
    Also, don't mix uint32_t and __u32 - use the same type throughout the
    file for consistency.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Conflicts:
    	arch/arm/include/asm/hardware/cache-l2x0.h
    	arch/arm/mm/cache-l2x0.c

--- arch/arm/include/asm/hardware/cache-l2x0.h
+++ arch/arm/include/asm/hardware/cache-l2x0.h
@@ -103,8 +103,8 @@
 #define L2X0_ADDR_FILTER_EN		1
 
 #ifndef __ASSEMBLY__
-extern void __init l2x0_init(void __iomem *base, __u32 aux_val, __u32 aux_mask);
-extern int l2x0_of_init(__u32 aux_val, __u32 aux_mask);
+extern void __init l2x0_init(void __iomem *base, u32 aux_val, u32 aux_mask);
+extern int l2x0_of_init(u32 aux_val, u32 aux_mask);
 
 struct l2x0_regs {
 	unsigned long phy_base;
