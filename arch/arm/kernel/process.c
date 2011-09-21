389ab52558515440aaadb85ef4800b16628cf325
Vladimir Zapolskiy <vzapolskiy@gmail.com>
2011-09-21 09:26:44 +0000

    connector: add comm change event report to proc connector
    Add an event to monitor comm value changes of tasks.  Such an event
    becomes vital, if someone desires to control threads of a process in
    different manner.
    
    A natural characteristic of threads is its comm value, and helpfully
    application developers have an opportunity to change it in runtime.
    Reporting about such events via proc connector allows to fine-grain
    monitoring and control potentials, for instance a process control daemon
    listening to proc connector and following comm value policies can place
    specific threads to assigned cgroup partitions.
    
    It might be possible to achieve a pale partial one-shot likeness without
    this update, if an application changes comm value of a thread generator
    task beforehand, then a new thread is cloned, and after that proc
    connector listener gets the fork event and reads new thread's comm value
    from procfs stat file, but this change visibly simplifies and extends the
    matter.
    
    Signed-off-by: Vladimir Zapolskiy <vzapolskiy@gmail.com>
    Acked-by: Evgeniy Polyakov <zbr@ioremap.net>
    Cc: David Miller <davem@davemloft.net>
    Signed-off-by: Andrew Morton <akpm@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Conflicts:
    	drivers/connector/cn_proc.c
    	include/linux/cn_proc.h

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -264,8 +264,7 @@ void cpu_idle(void)
 				cpu_relax();
 			} else if (!need_resched()) {
 				stop_critical_timings();
-				if (cpuidle_idle_call())
-					pm_idle();
+				pm_idle();
 				start_critical_timings();
 				/*
 				 * pm_idle functions must always
