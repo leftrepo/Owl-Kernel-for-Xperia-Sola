59d8ea417d101a0763651809a23f94baa956db1b
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2011-10-11 14:04:34 +0300

    Bluetooth: convert role_switch variable to flag in l2cap chan
    role_switch variable inside l2cap_chan is a logical one and can
    be easily converted to flag
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- include/net/bluetooth/l2cap.h
+++ include/net/bluetooth/l2cap.h
@@ -306,7 +306,6 @@ struct l2cap_chan {
 	__le16		sport;
 
 	__u8		sec_level;
-	__u8		role_switch;
 
 	__u8		ident;
 
@@ -445,6 +444,7 @@ enum {
 
 /* Definitions for flags in l2cap_chan */
 enum {
+	FLAG_ROLE_SWITCH,
 	FLAG_FORCE_ACTIVE,
 	FLAG_FORCE_RELIABLE,
 	FLAG_FLUSHABLE,
