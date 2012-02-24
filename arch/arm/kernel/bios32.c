02b37fa51edbf6da8e6031d6683fdeb030f8f12d
Bjorn Helgaas <bhelgaas@google.com>
2012-02-23 20:18:56 -0700

    arm/PCI: remove arch pci_flags definition
    The PCI core provides a pci_flags definition (currently __weak), so drop
    the arm definition in favor of that.
    
    We EXPORT_SYMBOL(pci_flags) as arm did previously.  I'm dubious about
    this: no other architecture exports it, and I didn't see any modules in
    the tree that reference it.
    
    CC: Rob Herring <rob.herring@calxeda.com>
    CC: Russell King <linux@arm.linux.org.uk>
    CC: linux-arm-kernel@lists.infradead.org
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- arch/arm/kernel/bios32.c
+++ arch/arm/kernel/bios32.c
@@ -537,6 +537,7 @@ void __init pci_common_init(struct hw_pci *hw)
 
 	INIT_LIST_HEAD(&hw->buses);
 
+	pci_add_flags(PCI_REASSIGN_ALL_RSRC);
 	if (hw->preinit)
 		hw->preinit();
 	pcibios_init_hw(hw);
@@ -571,6 +572,7 @@ void __init pci_common_init(struct hw_pci *hw)
 		pci_bus_add_devices(bus);
 	}
 }
+EXPORT_SYMBOL(pci_flags);
 
 char * __init pcibios_setup(char *str)
 {
