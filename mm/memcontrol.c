dbc8ccf937502c3b72009030b0959c9328ad3d12
Andrea Arcangeli <aarcange@redhat.com>
2012-03-28 14:42:40 -0700

    mm: thp: fix up pmd_trans_unstable() locations
    pmd_trans_unstable() should be called before pmd_offset_map() in the
    locations where the mmap_sem is held for reading.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Larry Woodman <lwoodman@redhat.com>
    Cc: Ulrich Obergfell <uobergfe@redhat.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Mark Salter <msalter@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	mm/memcontrol.c

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5125,6 +5125,8 @@ static int mem_cgroup_count_precharge_pte_range(pmd_t *pmd,
 	if (pmd_trans_unstable(pmd))
 		return 0;
 
+	if (pmd_trans_unstable(pmd))
+		return 0;
 	pte = pte_offset_map_lock(vma->vm_mm, pmd, addr, &ptl);
 	for (; addr != end; pte++, addr += PAGE_SIZE)
 		if (is_target_pte_for_mc(vma, addr, *pte, NULL))
@@ -5286,6 +5288,10 @@ static int mem_cgroup_move_charge_pte_range(pmd_t *pmd,
 	spinlock_t *ptl;
 
 	split_huge_page_pmd(walk->mm, pmd);
+	if (pmd_trans_unstable(pmd))
+		return 0;
+	}
+
 	if (pmd_trans_unstable(pmd))
 		return 0;
 retry:
