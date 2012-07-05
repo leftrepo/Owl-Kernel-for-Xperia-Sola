1c54aaa723ee532102464c2ae1e6abe7f6eb2448
Andy Lutomirski <luto@amacapital.net>
2012-07-05 16:00:11 -0700

    mm: Hold a file reference in madvise_remove
    commit 9ab4233dd08036fe34a89c7dc6f47a8bf2eb29eb upstream.
    
    Otherwise the code races with munmap (causing a use-after-free
    of the vma) or with close (causing a use-after-free of the struct
    file).
    
    The bug was introduced by commit 90ed52ebe481 ("[PATCH] holepunch: fix
    mmap_sem i_mutex deadlock")
    
    Cc: Hugh Dickins <hugh@veritas.com>
    Cc: Miklos Szeredi <mszeredi@suse.cz>
    Cc: Badari Pulavarty <pbadari@us.ibm.com>
    Cc: Nick Piggin <npiggin@suse.de>
    Signed-off-by: Andy Lutomirski <luto@amacapital.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [bwh: Backported to 3.2:
     - Adjust context
     - madvise_remove() calls vmtruncate_range(), not do_fallocate()]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: David Ng <dave@codeaurora.org>
    Patch-mainline: v3.5
    Git-commit: 9ab4233dd08036fe34a89c7dc6f47a8bf2eb29eb
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    
    Conflicts:
    	mm/madvise.c

--- mm/madvise.c
+++ mm/madvise.c
@@ -228,13 +228,12 @@ static long madvise_remove(struct vm_area_struct *vma,
 			+ ((loff_t)vma->vm_pgoff << PAGE_SHIFT);
 
 	/*
-	 * vmtruncate_range may need to take i_mutex and i_alloc_sem.
-	 * We need to explicitly grab a reference because the vma (and
-	 * hence the vma's reference to the file) can go away as soon as
-	 * we drop mmap_sem.
+	 * vmtruncate_range may need to take i_mutex.  We need to
+	 * explicitly grab a reference because the vma (and hence the
+	 * vma's reference to the file) can go away as soon as we drop
+	 * mmap_sem.
 	 */
 	get_file(f);
-	/* vmtruncate_range needs to take i_mutex */
 	up_read(&current->mm->mmap_sem);
 	error = vmtruncate_range(mapping->host, offset, endoff);
 	fput(f);
