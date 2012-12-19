1157ef444a1c69e774126c2d3c84ab5b711569bf
Maya Erez <merez@codeaurora.org>
2012-12-19 08:44:42 +0200

    mmc: core: activate bkops stats for eMMC4.41 cards
    BKOPS is a feature of eMMC4.41 cards and therefore is supported when
    ext csd revision is 5 and higher. We would like to gather bkops statistics
    for eMMC4.41 cards as well and need to check if the revision is higher
    than 5.
    
    Change-Id: Ic4471cae5892a459bed2d934137e8d01b3a46839
    Signed-off-by: Maya Erez <merez@codeaurora.org>
    
    Conflicts:
    	drivers/mmc/core/debugfs.c

--- drivers/mmc/core/debugfs.c
+++ drivers/mmc/core/debugfs.c
@@ -673,7 +673,11 @@ void mmc_add_card_debugfs(struct mmc_card *card)
 					 &mmc_dbg_wr_pack_stats_fops))
 			goto err;
 
-	if (mmc_card_mmc(card) && (card->ext_csd.rev >= 6) &&
+	if (!debugfs_create_file("new_req_stats", S_IRUSR, root, card,
+				&mmc_dbg_new_req_stats_fops))
+		goto err;
+
+	if (mmc_card_mmc(card) && (card->ext_csd.rev >= 5) &&
 	    card->ext_csd.bkops_en)
 		if (!debugfs_create_file("bkops_stats", S_IRUSR, root, card,
 					 &mmc_dbg_bkops_stats_fops))
