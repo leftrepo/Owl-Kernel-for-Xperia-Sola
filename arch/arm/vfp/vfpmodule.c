8d28b0831b5265ed803fff319e03f34681418d06
Laura Abbott <lauraa@codeaurora.org>
2013-03-14 19:13:49 -0700

    cma: Remove restriction on region names
    CMA currently restricts region names to 'region@x'. Devicetree
    does not support the same value of x to be used multiple times.
    This means that the devicetree cannot have multiple regions
    be dynamically placed (x = 0). Remove the naming restriction
    for CMA regions.
    
    Change-Id: If647f8d7e6323497952431ae5b8cae05ba17af50
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    
    Conflicts:
    	arch/arm/boot/dts/msm8974.dtsi

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -20,6 +20,7 @@
 #include <linux/init.h>
 #include <linux/uaccess.h>
 #include <linux/user.h>
+#include <linux/export.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
@@ -676,6 +677,26 @@ static int vfp_hotplug(struct notifier_block *b, unsigned long action,
 	return NOTIFY_OK;
 }
 
+#ifdef CONFIG_PROC_FS
+static int vfp_bounce_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%llu\n", atomic64_read(&vfp_bounce_count));
+	return 0;
+}
+
+static int vfp_bounce_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, vfp_bounce_show, NULL);
+}
+
+static const struct file_operations vfp_bounce_fops = {
+	.open		= vfp_bounce_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+#endif
+
 #ifdef CONFIG_KERNEL_MODE_NEON
 
 /*
@@ -721,26 +742,6 @@ void kernel_neon_end(void)
 EXPORT_SYMBOL(kernel_neon_end);
 
 #endif /* CONFIG_KERNEL_MODE_NEON */
-
-#ifdef CONFIG_PROC_FS
-static int vfp_bounce_show(struct seq_file *m, void *v)
-{
-	seq_printf(m, "%llu\n", atomic64_read(&vfp_bounce_count));
-	return 0;
-}
-
-static int vfp_bounce_open(struct inode *inode, struct file *file)
-{
-	return single_open(file, vfp_bounce_show, NULL);
-}
-
-static const struct file_operations vfp_bounce_fops = {
-	.open		= vfp_bounce_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
-};
-#endif
 
 /*
  * VFP support code initialisation.
