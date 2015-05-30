8b5946fcd237ca3c81b72f34c4e0a2620ce6dbaa
leftrepo <left-acc1@yandex.ua>
2015-05-31 00:29:38 +0300

    Подготовка block-dev к патчу.

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
-make -j3 ARCH=arm CROSS_COMPILE=/home/administrator/x-tools/arm-cortex_a9-linux-gnueabihf/bin/arm-cortex_a9-linux-gnueabihf-
+make -j3 ARCH=arm CROSS_COMPILE=~/x-tools/arm-cortex_a9-linux-gnueabihf/bin/arm-cortex_a9-linux-gnueabihf-
 cp -f ~/kernel/kernel_3.0.8/arch/arm/boot/zImage ~/boottools/tools
 cd ~/boottools/tools
 ./mkelf.py -o test.elf zImage@0x00008000 ramdisk.rec.lzma@0x1000000,ramdisk cmdline.txt@cmdline
