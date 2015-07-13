0e056431d3223aa95d93f4ef6e9fd5aa7fe943ad
leftrepo <left-acc1@yandex.ua>
2015-07-13 16:52:02 +0300

    Небольшая правка signal.c

--- kernel/signal.c
+++ kernel/signal.c
@@ -1997,6 +1997,8 @@ static bool do_signal_stop(int signr)
 		 */
 		if (!(sig->flags & SIGNAL_STOP_STOPPED))
 			sig->group_exit_code = signr;
+		else
+			WARN_ON_ONCE(!current->ptrace);
 
 		sig->group_stop_count = 0;
 
