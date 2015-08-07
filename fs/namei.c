d1197d1b6a56e26122fe7e23dfae477595574b5e
leftrepo <left-acc1@yandex.ua>
2015-08-07 15:03:25 +0300

    Prepere namei

--- fs/namei.c
+++ fs/namei.c
@@ -793,7 +793,7 @@ static int follow_automount(struct path *path, unsigned flags,
 	 * of the daemon to instantiate them before they can be used.
 	 */
 	if (!(flags & (LOOKUP_CONTINUE | LOOKUP_DIRECTORY |
-		     LOOKUP_OPEN | LOOKUP_CREATE | LOOKUP_AUTOMOUNT)) &&
+		     LOOKUP_OPEN | LOOKUP_CREATE)) &&
 	    path->dentry->d_inode)
 		return -EISDIR;
 
