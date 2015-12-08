f772314cb70a85ef2994bcd4121e19e3b206f5b3
leftrepo <left-acc1@yandex.ua>
2015-12-08 19:29:56 +0300

    Fix dma contiguous allocator

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -32,7 +32,8 @@
 #include <asm/sizes.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
-#include <asm/system_info.h>
+//#include <asm/system_info.h> - correct variant
+#include <asm/system.h>
 #include <asm/dma-contiguous.h>
 #include <asm/dma-iommu.h>
 
