e4bb44d9b9e167c4c96479d256251481105ea362
leftrepo <left-acc1@yandex.ua>
2015-12-19 16:17:30 +0300

    Minor cleanups

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -32,7 +32,7 @@
 #include <asm/sizes.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
-#include <asm/system_info.h> - correct variant
+#include <asm/system_info.h>
 #include <asm/dma-contiguous.h>
 #include <asm/dma-iommu.h>
 
